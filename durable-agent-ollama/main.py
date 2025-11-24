16a5ab41b3cf219a94259355ee22c7c15662348b
Casper Nielsen <casper@diagrid.io>
2025-11-23 19:46:48 -0800

    feat: init of dapr-agents durable agent with dapr conversation api and local ollama serving llama3.2
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

---
+++ durable-agent-ollama/main.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+
+from dotenv import load_dotenv
+
+
+from dapr_agents import DaprChatClient, DurableAgent  # type: ignore
+from dapr_agents.workflow.runners import AgentRunner # type: ignore
+
+
+async def main():
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    llm = DaprChatClient(component_name='ollama')
+
+    weather_agent = DurableAgent(
+        role="Personal Assistant",
+        name="Stevie",
+        goal="Help humans answer any question they may have.",
+        instructions=[
+            "Respond clearly and helpfully to any question.",
+        ],
+        llm=llm
+    )
+    weather_agent.start()
+    runner = AgentRunner()
+
+    try:
+        prompt = "How can I run ollama locally with Dapr Conversation API and dapr-agents durable agent?"
+
+        result = await runner.run(
+            weather_agent,
+            payload={"task": prompt},
+        )
+
+        print(f"\n✅ Final Result:\n{result}\n", flush=True)
+
+    except Exception as e:
+        logger.error(f"Error running workflow: {e}", exc_info=True)
+        raise
+    finally:
+        weather_agent.stop()
+        runner.shutdown()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
